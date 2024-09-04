c807a9508035b6c9a8e4c9ba5810448bde54c88b
unknown <kongnyuroger711@gmail.com>
2024-09-04 17:28:19 +0100

    changed cors to allow all origin

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');  // Import the cors package
 
-// Configure CORS to allow requests from localhost:3000
-app.use(cors({
-  origin: 'http://localhost:3000',  // Allow this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
-  credentials: true,  // Allow cookies
-}));
+// Configure CORS to allow all origin
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
