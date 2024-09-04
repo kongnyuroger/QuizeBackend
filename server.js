2699555452b1c4e4f0579c9fb6d8aefc83231f7f
unknown <kongnyuroger711@gmail.com>
2024-09-04 17:21:03 +0100

    changed cors origin back

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');  // Import the cors package
 
 // Configure CORS to allow requests from localhost:3000
 app.use(cors({
-  origin: '*',  // Allow this origin
+  origin: 'http://localhost:3000',  // Allow this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
   credentials: true,  // Allow cookies
 }));
